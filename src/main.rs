33263b8b554a706d2e4656349fd9941f742ae198
Simon Maghiar <43268629+SimonMaghiar@users.noreply.github.com>
2024-06-06 13:32:40 +0300

    Blockchain and transaction validity issues
    This commit improves the overall validity of the blockchain. 
    Initial coin offering to a MINT address is made. 
    Users are now able to get their balances. 
    Wallets are now static.
    A demo usage of the app is written in the main function.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use sha2::{Digest, Sha256};
 use secp256k1::{Message, PublicKey, Secp256k1};
 use secp256k1::ecdsa::Signature;
-use wallets::WALLET;
+use wallets::{Wallet, JOHN_PRIVATE_KEY, JENIFER_PRIVATE_KEY, MINER_PRIVATE_KEY, MINT_PRIVATE_KEY, PRE_RELEASE_PRIVATE_KEY};
 use std::{fmt::Write};
 use chrono::prelude::*;
-mod keypair;
 mod wallets;
 
 
@@ -49,6 +48,15 @@ impl Block {
             self.hash = self.get_hash();
         }
     }
+    fn has_valid_transactions(&self, chain: &Blockchain) -> bool {
+        for transaction in &self.transactions {
+            if !transaction.is_valid(chain) {
+                println!("Transaction that is not valid: {:?}", transaction);
+                return false;
+            }
+        }
+        return true;
+    }
 }
 #[derive(Debug)]
 
@@ -61,7 +69,7 @@ struct Transaction {
 
 impl Transaction {
     fn new (from: PublicKey, to: PublicKey, amount: f64) -> Self {
-        let mut transaction = Transaction {
+        let transaction = Transaction {
             from, 
             to, 
             amount,
@@ -69,26 +77,38 @@ impl Transaction {
         };
         return transaction;
     }
-    fn sign(&mut self, wallet: &WALLET) {
+    fn sign(&mut self, wallet: &Wallet) {
         if wallet.public_key == self.from {
             let secp = Secp256k1::new();
-            let message = Message::from_digest_slice(&[0xab; 32]).expect("32 bytes");
+            let message = self.create_message();
+            let message_hash = Sha256::digest(&message); // Create a SHA256 hash of the message
+            let message = Message::from_digest_slice(&message_hash).expect("32 bytes");
             self.signature = Some(secp.sign_ecdsa(&message, &wallet.private_key));
         }
     }
     fn create_message(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
         bytes.extend(&self.amount.to_le_bytes());
-        println!("{:?}", bytes);
-        return bytes;
+        bytes.extend(&self.from.serialize_uncompressed());
+        bytes.extend(&self.to.serialize_uncompressed());
+        bytes
     }
-
-    // fn verify(&self, public_key: &PublicKey) -> bool {
-    //     let context = Secp256k1::new();
-    //     let message = self.create_message();
-    //     let signature = Signature::from_str(&self.signature).unwrap();
-    //     context.verify(&Message::from_slice(&message).unwrap(), &signature, public_key).is_ok()
-    // }
+    fn is_valid(&self, chain: &Blockchain) -> bool {
+        if self.amount < 0.0 || chain.get_balance(self.from) < self.amount || self.signature == None || !self.verify() {
+            return false;
+        }
+        return true;
+    }
+    fn verify(&self) -> bool {
+        if let Some(signature) = self.signature {
+            let secp = Secp256k1::new();
+            let message = self.create_message();
+            let message_hash = Sha256::digest(&message); // Create a SHA256 hash of the message
+            let message = Message::from_digest_slice(&message_hash).expect("32 bytes");
+            return secp.verify_ecdsa(&message, &signature, &self.from).is_ok();
+        }
+        return false; // No signature or error occurred
+    }
 }
 
 
@@ -104,8 +124,14 @@ struct Blockchain {
 
 impl Blockchain {
     fn new() -> Self {
+        // Initial Coin Release
+        let pre_release_wallet = Wallet::new(PRE_RELEASE_PRIVATE_KEY);
+        let mint_wallet = Wallet::new(MINT_PRIVATE_KEY);
+        let john_wallet = Wallet::new(JOHN_PRIVATE_KEY);
+        let initial_coin_release_mint = Transaction::new(pre_release_wallet.public_key, mint_wallet.public_key, 10000000000000.0);
+        let initial_coin_release = Transaction::new(mint_wallet.public_key, john_wallet.public_key, 100.0);
         return Blockchain {
-            chain: vec![Block::new(vec![])],
+            chain: vec![Block::new(vec![initial_coin_release, initial_coin_release_mint])],
             transactions: vec![],
             difficulty: 2, 
             block_time: 5000,
@@ -113,26 +139,50 @@ impl Blockchain {
         }
     }
     fn add_transaction (&mut self, trasaction: Transaction) {
-        self.transactions.push(trasaction);
+        if trasaction.is_valid(&self) {
+            self.transactions.push(trasaction);
+        }
     }
+
+    fn get_balance (&self, address: PublicKey) -> f64 {
+        let mut balance: f64 = 0.0;
+        self.chain.iter().for_each(|block| {
+            block.transactions.iter().for_each(|transaction| {
+                if transaction.to == address {
+                    balance += transaction.amount;
+                }
+                if transaction.from == address {
+                    balance -= transaction.amount;
+                }
+            });
+        });
+        return balance;
+    }
+
     fn mine_transactions (&mut self, miner_reward_address: PublicKey) {
-        let mint_public_key = PublicKey::from_slice(&[0x02,0xc6, 0x6e, 0x7d, 0x89, 0x66, 0xb5, 0xc5, 0x55,0xaf, 0x58, 0x05, 0x98, 0x9d, 0xa9, 0xfb, 0xf8,0xdb, 0x95, 0xe1, 0x56, 0x31, 0xce, 0x35, 0x8c,0x3a, 0x17, 0x10, 0xc9, 0x62, 0x67, 0x90, 0x63]).expect("public keys must be 33 or 65 bytes, serialized according to SEC 2");
-        let reward_transaction = Transaction::new(mint_public_key, miner_reward_address, self.reward);
+        let mint_wallet = Wallet::new(MINT_PRIVATE_KEY);
+        let mut reward_transaction = Transaction::new(mint_wallet.public_key, miner_reward_address, self.reward);
+        reward_transaction.sign(&mint_wallet);
+
         let mut final_transactions = vec![reward_transaction];
         final_transactions.append(&mut self.transactions);
-        self.add_block(Block::new(final_transactions));
+        if final_transactions.len() > 1 {
+            self.add_block(Block::new(final_transactions));
+        } 
+        self.transactions.clear();
     }
+
     fn add_block(&mut self, mut block: Block) {
         block.previous_hash = self.chain.last().unwrap().hash.clone();
         block.mine(self.difficulty);
         self.chain.push(block);
     }
+
     fn is_valid (&self) -> bool {
         for i in 1..self.chain.len() {
             let current_block = &self.chain[i];
             let previous_block = &self.chain[i - 1];
-
-            if current_block.hash != current_block.get_hash() || current_block.previous_hash != previous_block.hash {
+            if current_block.hash != current_block.get_hash() || current_block.previous_hash != previous_block.hash || !current_block.has_valid_transactions(&self) {
                 return false;
             } 
         }
@@ -141,28 +191,17 @@ impl Blockchain {
 }
 
 fn main() {
-    let john_wallet: WALLET = WALLET::new();
-    let jenifer_wallet: WALLET = WALLET::new();
-    let miner_wallet: WALLET = WALLET::new();
-    
+    let john_wallet = Wallet::new(JOHN_PRIVATE_KEY);
+    let jenifer_wallet = Wallet::new(JENIFER_PRIVATE_KEY);
+    let miner_wallet = Wallet::new(MINER_PRIVATE_KEY);
+
     let mut blockchain = Blockchain::new();
-    let mut transaction = Transaction::new(john_wallet.public_key, jenifer_wallet.public_key, 100.0);
+    let mut transaction = Transaction::new(john_wallet.public_key, jenifer_wallet.public_key, 20.0);
     transaction.sign(&john_wallet);
-    // println!("{:#?}", transaction);
     blockchain.add_transaction(transaction);
     blockchain.mine_transactions(miner_wallet.public_key);
-    // let mut block1 = Block::new("First block".to_string());
-    // let mut block2 = Block::new("Second block".to_string());
-    // let mut block3 = Block::new("Third block".to_string());
 
-    // blockchain.add_block(block1);
-    // blockchain.add_block(block2);
-    // blockchain.add_block(block3);
-    // blockchain.chain[1].data = "This is hacked!".to_string();
-    println!("{:#?}", blockchain.chain);
-    // println!("Is the blockchain valid: {}", blockchain.is_valid());
-    // let (private_key, public_key) = keypair::generate_keypair().expect("Failed to generate key pair");
-    // Print the keys
-    // println!("Private key: {:?}", private_key);
-    // println!("Public key: {:?}", public_key);
+    println!("{:#?}", blockchain);
+    println!("John's balance: {}", blockchain.get_balance(john_wallet.public_key));
+    println!("Is blockchain valid: {}", blockchain.is_valid());
 }
--- src/wallets.rs
+++ src/wallets.rs
@@ -1,18 +1,29 @@
-use secp256k1::{SecretKey, PublicKey};
-use crate::keypair;
+use secp256k1::{Secp256k1, SecretKey, PublicKey};
 
-pub struct WALLET {
+pub const JOHN_PRIVATE_KEY: &str = "c87509a1c067bbde78beb793e6faafc2647b4db9ac64b0d1e3aa1c6c0bbecf7c";
+pub const JENIFER_PRIVATE_KEY: &str = "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f";
+pub const MINER_PRIVATE_KEY: &str = "b60e8dd61c5d32be8058bbd42a9254f5f01b1b6cb7eaa1f63b9b22d5af77b726";
+pub const MINT_PRIVATE_KEY: &str = "a14e8dd61c5d32be8058bbd42a9254f5f01b1b6cb7eaa1f63b9b22d5af1acf17";
+pub const PRE_RELEASE_PRIVATE_KEY: &str = "b14e8dd61c5d32be8058bbd42a9254f5f01b1b6cb7eaa1f63b9b22d5af1acf17";
+
+#[derive(Debug)]
+pub struct Wallet {
+    pub public_key: PublicKey,
     pub private_key: SecretKey,
-    pub public_key: PublicKey
 }
 
-impl WALLET {
-    pub fn new() -> Self  {
-        let (private_key, public_key) = keypair::generate_keypair().expect("Failed to generate key pair");
-        let mut wallet = WALLET {
+impl Wallet {
+    // Create a new wallet with the given private key
+    pub fn new(private_key_hex: &str) -> Self {
+        let secp = Secp256k1::new();
+        // Convert the hex string to bytes
+        let private_key_bytes = hex::decode(private_key_hex).expect("Hex decode failed");
+        let private_key = SecretKey::from_slice(&private_key_bytes).expect("32 bytes");
+        let public_key = PublicKey::from_secret_key(&secp, &private_key);
+        
+        Wallet {
+            public_key,
             private_key,
-            public_key
-        };
-        return wallet;
+        }
     }
 }
